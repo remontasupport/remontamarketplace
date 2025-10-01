8fee0552165c191be8f150ac662f83bcb50b26c0
unknown <bacatanclentvincent@gmail.com>
2025-10-01 06:29:39 +0530

    refactored the folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,20 @@
-import Hero from "@/components/sections/landing/Hero";
-import HowItWorks from "@/components/sections/landing/HowItWorks";
-import AboutUs from "@/components/sections/landing/AboutUs";
-import Testimonials from "@/components/sections/landing/Testimonials";
-import Services from "@/components/sections/landing/Services";
-import SearchSupport from "@/components/sections/landing/SearchSupport";
-import Pricing from "@/components/sections/landing/Pricing";
-import FAQ from "@/components/sections/landing/FAQ";
+import Hero from "@/app/pages/landing/Hero";
+import HowItWorks from "@/app/pages/landing/HowItWorks";
+import AboutUs from "@/app/pages/landing/AboutUs";
+import Testimonials from "@/app/pages/landing/Testimonials";
+import Services from "@/app/pages/landing/Services";
+import SearchSupport from "@/app/pages/landing/SearchSupport";
+import Pricing from "@/app/pages/landing/Pricing";
+import FAQ from "@/app/pages/landing/FAQ";
 import Footer from "@/components/ui/layout/Footer";
 // import AustraliaMap from "@/components/sections/landing/AustraliaMap";
+
+import LandingPage from "./pages/landing/page";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Hero />
-      <HowItWorks />
-      <AboutUs />
-      <Testimonials />
-      <Services />
-      {/* <SearchSupport /> */}
-      {/* <AustraliaMap /> */}
-      <Pricing />
-      <FAQ />
-      <Footer />
+     <LandingPage/>
     </div>
   );
 }
---
+++ src/app/pages/landing/page.tsx
@@ -0,0 +1,27 @@
+import Footer from "@/components/ui/layout/Footer";
+import AboutUs from "./AboutUs";
+import FAQ from "./FAQ";
+import Hero from "./Hero";
+import HowItWorks from "./HowItWorks";
+import Pricing from "./Pricing";
+import Services from "./Services";
+import Testimonials from "./Testimonials";
+
+
+
+export default function LandingPage() {
+  return (
+    <div className="min-h-screen">
+      <Hero />
+      <HowItWorks />
+      <AboutUs />
+      <Testimonials />
+      <Services />
+      {/* <SearchSupport /> */}
+      {/* <AustraliaMap /> */}
+      <Pricing />
+      <FAQ />
+      <Footer />
+    </div>
+  );
+}
